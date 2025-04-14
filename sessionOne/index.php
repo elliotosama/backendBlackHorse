f4a660b44422a7ff9be5d25967dba2bbbba92a2a
osama <osama@osama.osama>
2025-04-14 15:27:02 +0200

    first commit

---
+++ sessionOne/index.php
@@ -0,0 +1,49 @@
+<?php
+
+
+
+// single line comment
+
+/* multi
+ * line
+ * comment
+ */
+
+
+
+// print in php
+// $variable = "osama";
+
+// echo $variable;
+// print $variable;
+
+// $array = ['osama', 'ali', 'mohamed'];
+// print_r($array);
+
+
+// concatination
+
+
+// $firstName = "osama";
+// $lastName = "ramadan";
+
+// echo $firstName . $lastName;
+
+// making a new line
+// echo "<br>";
+
+
+
+
+// escape sequence
+
+// print the following string
+// hello from "black horse";
+
+
+// echo 'hello from "black horse"';
+// echo "hello from \"black horse\"";
+
+
+// getting informatino about the variable
+// echo var_dump($variable);
